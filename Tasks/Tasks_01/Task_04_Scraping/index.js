6b52fbed25c6eefa45cfde0598c6219588d37d36
Bastian Hodapp <bastianhodapp@gmail.com>
2022-03-21 16:23:55 +0100

    add comments

--- Tasks/Tasks_01/Task_04_Scraping/index.js
+++ Tasks/Tasks_01/Task_04_Scraping/index.js
@@ -4,6 +4,7 @@ import { JSDOM } from "jsdom";
 axios.get("https://www.spiegel.de/schlagzeilen/").then((response) => {
     const dom = new JSDOM(response.data);
 
+    // Convert from NodeList to Array using deconstructuring operator
     let allHeadlines = [
         ...dom?.window?.document?.querySelectorAll(
             "[data-block-el=articleTeaser]"
@@ -13,10 +14,12 @@ axios.get("https://www.spiegel.de/schlagzeilen/").then((response) => {
     let firstTenHeadlines = allHeadlines.slice(0, 10);
 
     firstTenHeadlines.forEach((eachHeadlineElement) => {
+        // Get the title of the article
         let articleTitle = eachHeadlineElement
             .querySelector("article")
             .getAttribute("aria-label");
 
+        // Get article time and/or date
         let timeAndOrDate = eachHeadlineElement.querySelector(
             ".items-end.leading-loose :first-child"
         ).innerHTML;
