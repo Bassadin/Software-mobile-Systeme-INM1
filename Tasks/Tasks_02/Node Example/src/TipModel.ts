973ee198c6ac7e55f8ff064a99aa31178f1ef941
Bastian Hodapp <bastianhodapp@gmail.com>
2022-03-28 15:05:30 +0200

    error handling for constructor parameters

--- Tasks/Tasks_02/Node Example/src/TipModel.ts
+++ Tasks/Tasks_02/Node Example/src/TipModel.ts
@@ -4,8 +4,22 @@ export default class TipModel {
     private grossAmount: number;
 
     constructor(partySize: number, tipPercentage: number, grossAmount: number) {
+        if (!Number.isInteger(partySize)) {
+            throw new Error("Party size is not an integer");
+        }
+        if (partySize < 1) {
+            throw new Error("Party size is smaller than 1");
+        }
         this.partySize = partySize;
+
+        if (tipPercentage < 0) {
+            throw new Error("Tip percentage is smaller than 0");
+        }
         this.tipPercentage = tipPercentage;
+
+        if (grossAmount < 0) {
+            throw new Error("Gross amount is smaller than 0");
+        }
         this.grossAmount = grossAmount;
     }
 
--- Tasks/Tasks_02/Node Example/tests/TipModel.test.ts
+++ Tasks/Tasks_02/Node Example/tests/TipModel.test.ts
@@ -1,13 +1,19 @@
 import TipModel from "../src/TipModel";
 import { expect } from "chai";
 
-describe("Options tests", () => {
-    it("checking default options", () => {
+describe("Tip Model tests", () => {
+    it("tests valid tip model calculations", () => {
         const tipModel = new TipModel(2, 5, 200);
-        
+
         expect(tipModel.getEntireTipSum()).to.equal(10);
         expect(tipModel.getGrossAmount()).to.equal(210);
         expect(tipModel.getGrossAmountPerPerson()).to.equal(105);
         expect(tipModel.getTipPerPerson()).to.equal(5);
+    });
+    it("tests negative party size", () => {
+        expect(() => new TipModel(-2, 5, 200)).to.throw(Error);
+    });
+    it("tests zero party size", () => {
+        expect(() => new TipModel(0, 5, 200)).to.throw();
     });
 });
