e2dcb676737c49c0eb90a812f741b1109693ce49
Bastian Hodapp <bastianhodapp@gmail.com>
2022-04-04 23:16:42 +0200

    logic for frontend

--- Tasks/Tasks_03/03_Tip_Calculator_MVC/src/scripts/app.ts
+++ Tasks/Tasks_03/03_Tip_Calculator_MVC/src/scripts/app.ts
@@ -1,7 +1,39 @@
 import TipModel from "./TipModel";
-const tipModel = new TipModel(2, 5, 200);
 
-console.log(tipModel.getEntireTipSum());
-console.log(tipModel.getGrossAmount());
-console.log(tipModel.getGrossAmountPerPerson());
-console.log(tipModel.getTipPerPerson());
+let calculateTipForm = document.getElementById("tip-calculation-form")!;
+
+let grossAmountInputField = <HTMLInputElement>(
+    document.getElementById("gross_amount")!
+);
+let tipPercentageInputField = <HTMLInputElement>(
+    document.getElementById("tip_percentage")!
+);
+let amountOfPersonsInputField = <HTMLInputElement>(
+    document.getElementById("amount_of_persons")!
+);
+
+let grossAmountResultItem = document.getElementById("gross-amount-result")!;
+let tipPerPersonResultItem = document.getElementById("tip-per-person-result")!;
+let entireTipSumResultItem = document.getElementById("entire-tip-sum-result")!;
+let grossAmountPerPersonResultItem = document.getElementById(
+    "gross-amount-per-person-result"
+)!;
+
+calculateTipForm.addEventListener("submit", (event) => {
+    // Keep browser from reloading after event submission
+    event.preventDefault();
+    const tipModel = new TipModel(
+        parseInt(amountOfPersonsInputField.value),
+        parseInt(tipPercentageInputField.value),
+        parseFloat(grossAmountInputField.value)
+    );
+
+    grossAmountResultItem.innerText =
+        tipModel.getGrossAmount().toFixed(2) + "€";
+    entireTipSumResultItem.innerText =
+        tipModel.getEntireTipSum().toFixed(2) + "€";
+    grossAmountPerPersonResultItem.innerText =
+        tipModel.getGrossAmountPerPerson().toFixed(2) + "€";
+    tipPerPersonResultItem.innerText =
+        tipModel.getTipPerPerson().toFixed(2) + "€";
+});
