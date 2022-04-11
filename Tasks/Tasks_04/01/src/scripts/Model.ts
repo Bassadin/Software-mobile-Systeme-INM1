064606cfcb1a3ecfdb327dcbe07a8437b4dd26d9
Bastian Hodapp <bastianhodapp@gmail.com>
2022-04-11 17:11:03 +0200

    formatting

--- Tasks/Tasks_04/01/src/scripts/Model.ts
+++ Tasks/Tasks_04/01/src/scripts/Model.ts
@@ -1,20 +1,17 @@
-
 import Observable from "./Patterns/Observable";
 import Observer from "./Patterns/Observer";
 
 export default class Model extends Observable<number> {
-	
-	private rate: number = 0.95;
-	private chf: number = 0;
+    private rate: number = 0.95;
+    private chf: number = 0;
 
-	public getEUR() {
-		let euro = this.chf * this.rate;
-		this.notifyObservers(euro);
-	}
+    public getEUR() {
+        let euro = this.chf * this.rate;
+        this.notifyObservers(euro);
+    }
 
-	public setCHF(amount: number): void {
-		this.chf = amount;
-		this.getEUR();
-	};
-
+    public setCHF(amount: number): void {
+        this.chf = amount;
+        this.getEUR();
+    }
 }
