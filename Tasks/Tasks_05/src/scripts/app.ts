46c42a47d15bd8b316cdbfc72e77124e57274699
Bastian Hodapp <bastianhodapp@gmail.com>
2022-04-25 23:16:51 +0200

    add first tailwind stylings

--- Tasks/Tasks_05/src/scripts/app.ts
+++ Tasks/Tasks_05/src/scripts/app.ts
@@ -28,25 +28,34 @@ document
             .then((response) => response.json())
             .then((data) => {
                 console.log(data);
-                outputCurrencyElement!.innerText =
-                    data.rates[targetCurrency].rate + " " + targetCurrency;
+                outputCurrencyElement!.innerText = `${data.rates[targetCurrency].rate_for_amount} ${targetCurrency} (Rate for currency ${targetCurrency} is ${data.rates[targetCurrency].rate})`;
             });
     });
 
 // Get available currencies for select item
-fetch(
-    `https://api.getgeoapi.com/v2/currency/list?api_key=${
-        import.meta.env.VITE_API_KEY
-    }&format=json`
-).then((response) => {
-    response.json().then((json) => {
-        Object.keys(json.currencies).forEach((eachCurrencyKey: string) => {
-            let newOptionElement: HTMLOptionElement =
-                document.createElement("option");
-            newOptionElement.setAttribute("value", eachCurrencyKey);
-            newOptionElement.innerText = json.currencies[eachCurrencyKey];
+function getAvailableCurrenciesAndSetDefaultOne() {
+    fetch(
+        `https://api.getgeoapi.com/v2/currency/list?api_key=${
+            import.meta.env.VITE_API_KEY
+        }&format=json`
+    ).then((response) => {
+        response.json().then((json) => {
+            const currencies: any = json.currencies;
 
-            targetCurrencyElement?.appendChild(newOptionElement);
+            Object.keys(currencies).forEach((eachCurrencyKey: string) => {
+                let newOptionElement: HTMLOptionElement =
+                    document.createElement("option");
+                newOptionElement.setAttribute("value", eachCurrencyKey);
+                newOptionElement.innerText = `${currencies[eachCurrencyKey]} (${eachCurrencyKey})`;
+
+                targetCurrencyElement?.appendChild(newOptionElement);
+            });
+            // Set preselected item
+            document
+                .querySelector("option[value='CHF']")
+                ?.setAttribute("selected", "selected");
         });
     });
-});
+}
+
+getAvailableCurrenciesAndSetDefaultOne();
