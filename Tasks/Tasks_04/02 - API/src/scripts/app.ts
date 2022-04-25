e91654c47c8ad8307b13e4fcc3fd28a1c019a793
Bastian Hodapp <bastianhodapp@gmail.com>
2022-04-25 18:05:05 +0200

    use api and get things working

--- Tasks/Tasks_04/02 - API/src/scripts/app.ts
+++ Tasks/Tasks_04/02 - API/src/scripts/app.ts
@@ -6,29 +6,51 @@ var controller = new Controller(model);
 
 model.addObserver(controller);
 
-let sourceCurrencyElement = document.getElementById("sourceCurrency");
-let targetCurrencyElement = document.getElementById("targetCurrency");
+let targetCurrencyElement: HTMLSelectElement = <HTMLSelectElement>(
+    document.getElementById("targetCurrency")
+);
+
+let currencyInputElement: HTMLInputElement = <HTMLInputElement>(
+    document.getElementById("currencyInput")
+);
+
+let outputCurrencyElement = document.getElementById("outputCurrency");
 
 document
     .getElementById("conversionForm")!
     .addEventListener("submit", (event) => {
         event.preventDefault();
-        controller.processCHF();
+        // controller.processCHF();
 
-        
+        let targetCurrency = targetCurrencyElement.value;
+        let amountToConvert = parseFloat(currencyInputElement.value);
+
+        fetch(
+            `https://api.getgeoapi.com/v2/currency/convert?api_key=${
+                import.meta.env.VITE_API_KEY
+            }&from=EUR&to=${targetCurrency}&amount=${amountToConvert}&format=json`
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                outputCurrencyElement!.innerText =
+                    data.rates[targetCurrency].rate + " " + targetCurrency;
+            });
     });
 
-
-fetch("https://openexchangerates.org/api/currencies.json").then((response) => {
+fetch(
+    `https://api.getgeoapi.com/v2/currency/list?api_key=${
+        import.meta.env.VITE_API_KEY
+    }&format=json`
+).then((response) => {
     response.json().then((json) => {
-        Object.keys(json).forEach((eachCurrencyKey: string) => {
+        Object.keys(json.currencies).forEach((eachCurrencyKey: string) => {
             let newOptionElement: HTMLOptionElement =
                 document.createElement("option");
             newOptionElement.setAttribute("value", eachCurrencyKey);
-            newOptionElement.innerText = json[eachCurrencyKey];
+            newOptionElement.innerText = json.currencies[eachCurrencyKey];
 
-            sourceCurrencyElement?.appendChild(newOptionElement.cloneNode(true));
-            targetCurrencyElement?.appendChild(newOptionElement.cloneNode(true));
+            targetCurrencyElement?.appendChild(newOptionElement);
         });
     });
 });
