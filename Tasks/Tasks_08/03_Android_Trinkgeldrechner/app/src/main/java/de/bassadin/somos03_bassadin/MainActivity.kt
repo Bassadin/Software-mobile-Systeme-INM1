391e8c367be1b21d48f35374ddacfbeff2aa793f
Bastian Hodapp <bastianhodapp@gmail.com>
2022-05-27 12:44:27 +0200

    some textinput stuff

--- Tasks/Tasks_08/03_Android_Trinkgeldrechner/app/src/main/java/de/bassadin/somos03_bassadin/MainActivity.kt
+++ Tasks/Tasks_08/03_Android_Trinkgeldrechner/app/src/main/java/de/bassadin/somos03_bassadin/MainActivity.kt
@@ -4,26 +4,34 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var resultTextView: TextView
-    lateinit var sourceCurrencyInput: EditText
-    lateinit var convertButton: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        resultTextView = findViewById<TextView>(R.id.resultTextView);
-        sourceCurrencyInput = findViewById<EditText>(R.id.editTextNumberDecimal_srcCurrency);
-        convertButton = findViewById<Button>(R.id.convert_button);
+        var grossAmountInput = findViewById<EditText>(R.id.grossAmountInput);
+        var tipPercentageInput = findViewById<EditText>(R.id.tipPercentageInput);
+        var personsAmountInput = findViewById<EditText>(R.id.personsAmountInput);
+
+        var convertButton = findViewById<Button>(R.id.convert_button);
+
+        var resultTextView = findViewById<TextView>(R.id.resultTextView);
 
         convertButton.setOnClickListener {
-            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+
+            val grossAmountInputValue = grossAmountInput.text.toString().toFloat();
+            val tipPercentageInputValue = grossAmountInput.text.toString().toFloat();
+            val personsAmountInputValue = grossAmountInput.text.toString().toInt();
+
+            
+
+
+            resultTextView.text = outputValue.toString();
+
         }
 
     }
