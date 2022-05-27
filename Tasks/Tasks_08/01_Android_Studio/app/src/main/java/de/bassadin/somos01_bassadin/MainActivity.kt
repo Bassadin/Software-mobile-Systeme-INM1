27320cfd79c8cebed5008b635f0600b6c86713ba
Bastian Hodapp <bastianhodapp@gmail.com>
2022-05-27 10:26:18 +0200

    finish task 2

--- Tasks/Tasks_08/01_Android_Studio/app/src/main/java/de/bassadin/somos01_bassadin/MainActivity.kt
+++ Tasks/Tasks_08/01_Android_Studio/app/src/main/java/de/bassadin/somos01_bassadin/MainActivity.kt
@@ -2,10 +2,22 @@ package de.bassadin.somos01_bassadin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState);
+        var webView: WebView = WebView(this);
+
+        webView.webViewClient = WebViewClient();
+        var webSettings = webView.settings;
+
+        webSettings.javaScriptEnabled = true;
+
+        // webView.loadUrl("https://m.spiegel.de");
+        webView.loadUrl("file:///android_asset/index.html");
+
+        setContentView(webView);
     }
 }
