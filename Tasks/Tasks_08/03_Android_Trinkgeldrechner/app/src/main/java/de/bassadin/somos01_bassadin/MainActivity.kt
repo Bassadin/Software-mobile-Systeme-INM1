4c7352f7eeda24c809318ce65cbacbe1332dbc73
Bastian Hodapp <bastianhodapp@gmail.com>
2022-05-27 11:01:01 +0200

    files for task 3

---
+++ Tasks/Tasks_08/03_Android_Trinkgeldrechner/app/src/main/java/de/bassadin/somos01_bassadin/MainActivity.kt
@@ -0,0 +1,13 @@
+package de.bassadin.somos01_bassadin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
