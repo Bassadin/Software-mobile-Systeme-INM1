86967df0905632479cdb2b94eaa235d2516476b4
Bastian Hodapp <bastianhodapp@gmail.com>
2022-05-27 11:47:21 +0200

    view somwhat working

--- Tasks/Tasks_08/03_Android_Trinkgeldrechner/app/src/main/java/de/bassadin/somos03_bassadin/MainActivity.kt
+++ Tasks/Tasks_08/03_Android_Trinkgeldrechner/app/src/main/java/de/bassadin/somos03_bassadin/MainActivity.kt
@@ -6,22 +6,26 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import de.bassadin.somos03_bassadin.R
 
 class MainActivity : AppCompatActivity() {
 
-    var resultTextView: TextView = findViewById<TextView>(R.id.resultTextView);
-    var sourceCurrencyInput: EditText =
-        findViewById<EditText>(R.id.editTextNumberDecimal_srcCurrency);
-    var convertButton: Button = findViewById<Button>(R.id.convert_button);
+    lateinit var resultTextView: TextView
+    lateinit var sourceCurrencyInput: EditText
+    lateinit var convertButton: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        resultTextView = findViewById<TextView>(R.id.resultTextView);
+        sourceCurrencyInput = findViewById<EditText>(R.id.editTextNumberDecimal_srcCurrency);
+        convertButton = findViewById<Button>(R.id.convert_button);
+
         convertButton.setOnClickListener {
             Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
         }
+
     }
 
 
