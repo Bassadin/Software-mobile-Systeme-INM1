0f2cc741cd18fdf8160df315145c30daac22fe70
Bastian Hodapp <bastianhodapp@gmail.com>
2022-05-27 09:52:38 +0200

    add files for android (task 8)

---
+++ Tasks/Tasks_08/01_Android_Studio/app/src/main/java/de/bassadin/somos01_bassadin/MainActivity.kt
@@ -0,0 +1,11 @@
+package de.bassadin.somos01_bassadin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
